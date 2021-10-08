d4dd7ecb8b472b5d4d3fd38c734d2b7fb557cb32
Sugar Shane <scrisostomo17@gmail.com>
2021-10-08 14:46:19 -0400

    working on main page - no image still need to connect

---
+++ components/About/index.js
@@ -0,0 +1,32 @@
+
+   
+import React, { useState } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+
+function About() {
+    const [pages] = useState([
+        {
+            name: "about me"
+        }
+    ]);
+    const [currentPage] = useState(pages[0]);
+
+    return (
+        <div className="masthead text-center text-white d-flex">
+            <div className="container my-auto">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto">
+                        <h1>{capitalizeFirstLetter(currentPage.name)}</h1>
+                        <hr />
+                        <p className="text-faded mb-5">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                       
+                        <p className="text-faded mb-5">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                        {/* <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about">Check Out My Work</a> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
